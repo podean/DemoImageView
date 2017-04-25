a1f7ce1c0906a5ce4d3482cba9de9d0bd1a1942c
15017167 <podean0957@gmail.com>
2017-04-25 10:35:09 +0800

    updated image

--- app/src/main/java/com/example/potran/demoimageview/MainActivity.java
+++ app/src/main/java/com/example/potran/demoimageview/MainActivity.java
@@ -2,12 +2,27 @@ package com.example.potran.demoimageview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView ivDay2, ivDay3, ivDay4, ivDay5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ivDay2 = (ImageView) findViewById(R.id.imageView2);
+        ivDay3 = (ImageView) findViewById(R.id.imageView3);
+        ivDay4 = (ImageView) findViewById(R.id.imageView4);
+        ivDay5 = (ImageView) findViewById(R.id.imageView5);
+
+        //set the image
+        ivDay2.setImageResource(R.drawable.day2);
+        ivDay3.setImageResource(R.drawable.day3);
+        ivDay4.setImageResource(R.drawable.day4);
+        ivDay5.setImageResource(R.drawable.day5);
+
     }
 }
